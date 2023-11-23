fc75b74e6d1111db546c119845eb449fe3fbfcf5
iasarchi <iasmimcardoso11@gmail.com>
2023-11-23 14:59:59 -0300

    Exercicios Listas

---
+++ ListaDeTarefa.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaDeTarefa {
+    private List<Tarefa> tarefaList;
+
+    public ListaDeTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa tarefa : tarefaList){
+            if (tarefa.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(tarefa);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+    }
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaDeTarefa listaTarefa = new ListaDeTarefa();
+        System.out.println(listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Teste 1");
+
+        System.out.println(listaTarefa.obterNumeroTotalTarefas());
+    }
+}
+
